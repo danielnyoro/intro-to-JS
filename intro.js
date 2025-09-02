0590bfd512ce344d42b69ea01b48fa0b5aeae84c
Daniel N N <danielnyoro7@gmail.com>
2025-09-02 11:05:18 +0300

    intro to Js

---
+++ intro.js
@@ -0,0 +1,20 @@
+// syntax - rules associated with a programming language
+// execution order - left to right and top to bottom
+
+/**
+ * comments do not get executed in any language
+ * this is a multiline comment.
+ * 
+ * 
+ */
+/**
+ * expression  - combination of numbers and variables and does not contain any equal signs.
+ * expression - Combination of data using symbols  - PEDMAS (+.-.*,**)
+ * Evaluation - When Javascript reads an expression and returns the value(constant vlue that doesnt change)
+/** PEDMAS (+.-.*,**)
+ * //command palette - ctrl + shift +P
+ */
+
+console.log(1+1)// 2
+ console.log(2+(3-5))//9
+ 
