093d7007db92434229b8ca6fe777daaaa13000ad
Daniel N N <danielnyoro7@gmail.com>
2025-09-02 11:17:36 +0300

    Modified Intro to JS

--- intro.js
+++ intro.js
@@ -16,5 +16,29 @@
  */
 
 console.log(1+1)// 2
- console.log(2+(3-5))//9
- 
+console.log(2+(3-5))// 10
+/**
+ * Assignment - asigning a value to a variable
+ * we use = operator (equal sign we call it assisgnement operator)
+ * on left we have the variable and the right we have the value
+ * variable is a container to store values for later retrieval
+ */
+
+firstName = "Hidaya"
+lastName="owuor"
+studentName="Jane Doe"
+age ="18"
+
+/** lookup - expression */
+console.log(firstname)
+console.log(lastname)
+
+
+/** 
+ * Rules for Creating variables
+ * 1. should use the camelcase naming convention 
+ * 2. JS is case sensitive
+ * 3. Don"t Start variables with numbers, always start start with lowercase letter.
+ * 4. Do not use reserved keywords( words used by the programming language)
+ * 5. As a rule of the thumb always use const or let to create variables.
+*/
