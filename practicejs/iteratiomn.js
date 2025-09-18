fd0ce92179ea3b4a715f6a55f64f04c5ef43a2fc
Daniel N N <danielnyoro7@gmail.com>
2025-09-18 04:53:08 +0300

    Updates

---
+++ practicejs/iteratiomn.js
@@ -0,0 +1,10 @@
+
+
+const numbers = [10, 15, 20, 25, 30];
+
+const twenties = numbers.filter(numbers => numbers number => 20);
+
+console.log(twenties)
+
+
+
---
+++ practicejs/script.js
@@ -0,0 +1,17 @@
+function isRose(cat){
+    if (cat === 'rose'){
+        return 'Hi Rose you are cute !'
+    } else {
+        return `Hi ${cat}!`
+    } 
+}
+
+function isEsther(lady){
+    if (lady === 'esther'){
+        return 'Hi Esther my Love !'
+    } else if (typeoflady === 'string'){
+        return 'Hi Madam'
+    } else {
+        return 'Please input lady not any other Sex'
+    }
+}
